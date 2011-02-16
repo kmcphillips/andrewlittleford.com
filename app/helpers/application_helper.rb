256057a54b4b7ff85b0cfead381e388c508dc5cb
Kevin McPhillips <github@kevinmcphillips.ca>
2011-02-15 21:48:02 -0600

    Colour, layout, forms, and lots of great UI stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -31,7 +31,7 @@ module ApplicationHelper
 
   def destroy_entity_image(path, label=nil, args={})
     path = polymorphic_path(path) if path.is_a?(Array)
-    html = link_to image_tag("/images/icons/delete.png", :alt => "Delete"), path, :method => :delete, :confirm => "Are you sure you want to delete this?", :title => "Delete", :class => ""
+    html = link_to image_tag("/images/icons/delete.png", :alt => "Delete"), path, :method => :delete, :confirm => "Are you sure you want to delete this?", :title => "Delete", :class => "action-image"
     html += "&nbsp;".html_safe + link_to(label, path, :method => :delete, :confirm => "Are you sure you want to delete this?") if label
     html
   end
