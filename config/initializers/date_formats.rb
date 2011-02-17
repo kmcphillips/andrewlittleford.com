7dda2b8c474f462ef96f6bb9f3df46298be6d374
Kevin McPhillips <github@kevinmcphillips.ca>
2011-02-16 21:15:18 -0600

    Added JS to do the fancy datetime picker stuff all client side.

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,5 +1,4 @@
 Time::DATE_FORMATS[:news_without_time] = lambda{ |time| time.strftime("%B #{time.day.ordinalize} %Y") }
 Time::DATE_FORMATS[:event_with_time] = lambda{ |time| time.strftime("%B #{time.day.ordinalize} %Y at %I:%M%p") }
 Time::DATE_FORMATS[:index] = "%b %e/%Y %I:%M%p"
-#Time::DATE_FORMATS[:form] = "%Y/%m/%d %I:%m %p"
-Time::DATE_FORMATS[:form] = "%d/%m/%Y %I:%m %p"
+Time::DATE_FORMATS[:form] = lambda{ |time| time.strftime("%d/%m/%Y %I:%m %p").upcase }
--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  
+
 });
 
 function stripe_table(table)
@@ -12,4 +12,66 @@ function stripe_table(table)
     else
       $(element).addClass("even");
   });
+}
+
+function convert_datetime_select(id)
+{
+    year = 0;
+    month = 0;
+    day = 0;
+
+    $(id).find("select").each(function(index, select){
+        switch(index)
+        {
+            case 0: // year
+                year = $(select).val();
+                $(select).hide();
+                break;
+            case 1: // month
+                month = $(select).val();
+                $(select).hide();
+                break;
+            case 2: // day
+                day = $(select).val();
+                $(select).hide();
+                break;
+            case 3: // hour
+                selected = $(select).val();
+                $(select).children().remove()
+                for(i = 0; i <= 23; i++)
+                {
+                    if(i == 0)
+                        text = "AM 12"
+                    else if(i < 12)
+                        text = "AM " + i
+                    else if(i == 12)
+                        text = "PM 12"
+                    else
+                        text = "PM " + (i - 12)
+
+                    $(select).append("<option value='" + i + "' " + (selected == i ? "selected='selected'" : "") + ">" + text + "</option>")
+                }
+                break;
+            case 4: // minute
+                $(select).find("option").each(function(){
+                    if($(this).val() != "00" && $(this).val() != "15" && $(this).val() != "30" && $(this).val() != "45")
+                        $(this).remove();
+                });
+        }
+    });
+
+    $(id).prepend("<input type='text' size='10' class='datetime_picker' value='" + year + "/" + month + "/" + day + "' />");
+    $(id).find("input[type=text]").datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: "yy/mm/dd",
+        onClose: function(text) {
+            tokens = text.split("/");
+            for(i = 0; i <= 2; i++)
+            {
+                $($(id).find("select")[i]).val(tokens[i].replace(/^[0]/g,""));
+            }
+        }
+    });
+
 }
