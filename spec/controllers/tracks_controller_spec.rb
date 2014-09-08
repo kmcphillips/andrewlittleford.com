28ca9a10156b47d3efbe87e2e8fe40adbfb5625b
Kevin McPhillips <github@kevinmcphillips.ca>
2014-09-08 18:41:28 -0400

    Refactor tracks controller

--- spec/controllers/tracks_controller_spec.rb
+++ spec/controllers/tracks_controller_spec.rb
@@ -1,18 +1,13 @@
 require 'spec_helper'
 
 describe TracksController do
-
-  def mock_track(stubs={})
-    @mock_track ||= mock_model(Track, stubs).as_null_object
-  end
+  let(:track){ FactoryGirl.create(:track) }
 
   describe "GET show" do
     it "assigns the requested track as @track" do
-      Track.stub(:find).with("37") { mock_track }
-      Track.stub(:order) { [mock_track] }
-      get :show, :id => "37"
-      assigns(:track).should be(mock_track)
-      assigns(:tracks).should == [mock_track]
+      get :show, id: track.id
+      expect(assigns(:track)).to eq(track)
+      expect(assigns(:tracks)).to eq([track])
     end
   end
 
