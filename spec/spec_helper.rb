bae67429654b5e614722d94501095bde8bc6a367
Kevin McPhillips <github@kevinmcphillips.ca>
2014-09-08 18:40:59 -0400

    Infer type from location

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.raise_errors_for_deprecations!
+  config.infer_spec_type_from_file_location!
 end
 
 def login_as_mock_user
