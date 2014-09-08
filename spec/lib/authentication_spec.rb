de076d0cc3babc085c3f1ed73dfb4fbb100a315a
Kevin McPhillips <github@kevinmcphillips.ca>
2014-09-08 18:35:53 -0400

    Test authentication

--- spec/lib/authentication_spec.rb
+++ spec/lib/authentication_spec.rb
@@ -4,49 +4,37 @@ describe Authentication do
   before(:each) do
     # This doesn't pass the smell test but it works
     @controller = Object.new.extend Authentication
-    @controller.stub(:session => {})
+    allow(@controller).to receive(:session).and_return({})
   end
 
   describe "current_user" do
-    before(:each) do
-      @u = User.create! :username => "km", :name => "Kevin McPhillips", :email => "km@kevinmcphillips.ca", :password_hash => "pie"
-    end
+    let!(:user){ FactoryGirl.create(:user) }
 
     describe "with a logged in user" do
       it "should find the user" do
-        @controller.session[:current_user] = @u.id
-        @controller.current_user.should == @u
-      end
-
-      it "should find the user" do
-        User.should_not_receive(:find_by_id)
-        @controller.current_user(@u)
-        @controller.current_user
+        @controller.session[:current_user] = user.id
+        expect(@controller.current_user).to eq(user)
       end
 
       it "should set the user" do
-        @controller.current_user(@u)
-        @controller.current_user.should == @u
+        @controller.current_user(user)
+        expect(@controller.current_user).to eq(user)
       end
 
       it "should raise if you call it with something else" do
-        lambda{ @controller.current_user(double) }.should raise_error
+        expect(->{ @controller.current_user(double) }).to raise_error
       end
     end
 
     describe "without a logged in user" do
       it "should not find it" do
-        @controller.current_user.should be_nil
+        expect(@controller.current_user).to be_nil
       end
 
       it "should set the user" do
-        @controller.current_user(@u)
-        @controller.session[:current_user].should == @u.id
+        @controller.current_user(user)
+        expect(@controller.session[:current_user]).to eq(user.id)
       end
-    end
-
-    after(:each) do
-      User.delete_all
     end
   end
 
@@ -54,7 +42,7 @@ describe Authentication do
     it "should clear it out" do
       @controller.session[:current_user] = "pie"
       @controller.clear_current_user
-      @controller.current_user.should be_nil
+      expect(@controller.current_user).to be_nil
     end
   end
 
