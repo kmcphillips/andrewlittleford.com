908a92e63e391d6da01a84d981ea26f500cc4e89
Kevin McPhillips <github@kevinmcphillips.ca>
2014-09-08 23:06:28 -0400

    Convert admin gallery controller

--- app/controllers/admin/galleries_controller.rb
+++ app/controllers/admin/galleries_controller.rb
@@ -1,8 +1,6 @@
 class Admin::GalleriesController < Admin::ApplicationController
-
   def index
     @gallery = Gallery.singleton
     @title = "Gallery - #{@gallery.name}"
   end
-
 end
--- spec/controllers/admin/galleries_controller_spec.rb
+++ spec/controllers/admin/galleries_controller_spec.rb
@@ -3,16 +3,15 @@ require 'spec_helper'
 describe Admin::GalleriesController do
   before(:each) do
     login_as_mock_user
-    @image = mock_model(Image)
-    @gallery = mock_model(Gallery, :name => "delicious pie")
   end
+
+  let!(:image){ FactoryGirl.create(:image) }
+  let!(:gallery){ image.gallery }
 
   describe "GET index" do
     it "should load and assign all galleries" do
-      Gallery.should_receive(:singleton).and_return(@gallery)
       get :index
-      assigns(:title).should be_an_instance_of(String)
-      assigns(:gallery).should == @gallery
+      expect(assigns(:gallery)).to eq(gallery)
     end
   end
 end
