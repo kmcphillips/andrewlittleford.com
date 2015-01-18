dce410b953a8dfe65730becb559f113e41c6abdd
Kevin McPhillips <github@kevinmcphillips.ca>
2015-01-18 18:52:34 -0500

    Date format tweak

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,5 +1,5 @@
 Time::DATE_FORMATS[:news_without_time] = lambda{ |time| time.strftime("%B #{time.day.ordinalize} %Y") }
 Time::DATE_FORMATS[:event_with_time] = lambda{ |time| time.strftime("%B #{time.day.ordinalize} %Y at %I:%M%p") }
-Time::DATE_FORMATS[:index] = "%b %e/%Y %I:%M%p"
+Time::DATE_FORMATS[:index] = "%b %e, %Y %I:%M%p"
 Time::DATE_FORMATS[:form] = lambda{ |time| time.strftime("%d/%m/%Y %I:%m %p").upcase }
 Time::DATE_FORMATS[:datetimepicker] = "%Y/%m/%d %H:%m"
