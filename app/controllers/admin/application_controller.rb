d48256f7d280eb49dc5046d94724976c45bb843d
Kevin McPhillips <github@kevinmcphillips.ca>
2015-02-16 17:45:59 -0500

    Add a read-only mode

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,17 @@
 class Admin::ApplicationController < ApplicationController
-  before_filter :require_login
+  before_filter :require_login, :check_read_only
+
+  def read_only?
+    !!Rails.application.secrets[:read_only]
+  end
+
+  def check_read_only
+    if read_only?
+      clear_current_user
+      flash[:error] = "Sorry, but you cannot log in right now. Site temporarily in read-only mode."
+      redirect_to root_path
+    end
+
+    true
+  end
 end
