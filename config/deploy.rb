174d0ffad0b70f112a3a776008959eb5792b5330
Kevin McPhillips <github@kevinmcphillips.ca>
2017-01-29 16:58:17 -0500

    Upgrade to cap 3 and new server deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,47 @@
-require "bundler/capistrano"
-require "rvm/capistrano"
+# config valid only for current version of Capistrano
+lock '3.6.1'
 
-set :application, "Andrew Littleford"
-set :repository,  "git://github.com/kmcphillips/andrewlittleford.com.git"
-set :deploy_to, "/home/kevin/andrewlittleford.com"
-set :user, "kevin"
-set :use_sudo, false
-set :scm, "git"
-set :keep_releases, 5
+set :application, 'andrewlittleford.com'
+set :repo_url, 'https://github.com/kmcphillips/andrewlittleford.com.git'
 
-default_run_options[:pty] = true
+set :user, "deploy"
+set :rbenv_ruby, "2.3.1"
 
-role :web, "198.211.110.159"
-role :app, "198.211.110.159"
-role :db,  "198.211.110.159", primary: true
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, roles: :app, except: { no_release: true } do
-    run "#{ try_sudo } touch #{ File.join(current_path,'tmp','restart.txt') }"
-  end
-end
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
 
-after "deploy:update", "deploy:cleanup"
+# Default value for :scm is :git
+# set :scm, :git
 
-before "deploy:finalize_update", "symlink_shared_files"
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-task :symlink_shared_files do
-  run "ln -s #{ shared_path }/attachments #{ release_path }/public/attachments"
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
-  %w{secrets.yml}.each do |config|
-    run "rm #{ release_path }/config/#{ config }"
-  end
+# Default value for :pty is false
+# set :pty, true
 
-  %w{database.yml secrets.yml}.each do |config|
-    run "ln -s #{ shared_path }/#{ config } #{ release_path }/config/#{config}"
-  end
-end
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/attachments'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 2
+
+# Unicorn
+# set :unicorn_pid, -> { File.join(current_path, "tmp", "pids", "unicorn.pid") }
+set :unicorn_config_path, -> { File.join(current_path, "config", "unicorn.rb") }
+# set :unicorn_roles, -> { :app }
+# set :unicorn_options, -> { "" }
+# set :unicorn_rack_env, -> { fetch(:rails_env) == "development" ? "development" : "deployment" }
+# set :unicorn_restart_sleep_time, 3
